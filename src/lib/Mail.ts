0df3d5b24a349ab85ba02081092ee1b541e55c54
henrythcampos@gmail.com <henry.campos@estudante.ifms.edu.br>
2026-03-11 19:14:20 -0300

    feat(main): Arruma os erros de Redis e Mail

--- src/lib/Mail.ts
+++ src/lib/Mail.ts
@@ -3,7 +3,7 @@ import mailConfig from "../config/mail.js";
 
 class Mail {
   constructor() {
-    this.resend = new Resend(process.env.RESEND_API_KEY);
+    this.resend = new Resend(process.env.MAIL_API_TOKEN);
   }
 
   async send({ to, subject, html, text }) {
