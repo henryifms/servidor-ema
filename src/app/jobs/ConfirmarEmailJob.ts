65af30210247117fe93284f3656b9b8389d8f6f3
henrythcampos@gmail.com <henry.campos@estudante.ifms.edu.br>
2026-03-31 13:15:18 -0300

    feat(main): Enviando funcionalidades de hoje.

--- src/app/jobs/ConfirmarEmailJob.ts
+++ src/app/jobs/ConfirmarEmailJob.ts
@@ -8,7 +8,7 @@ class ConfirmarEmailJob {
   async handle({ data }) {
     const { nome, email, token } = data;
 
-    const link = `http://localhost:5173/confirmar-email?token=${token}`;
+    const link = `http://localhost:3000/confirmar-email?token=${token}`;
 
     await Mail.send({
       to: email,
